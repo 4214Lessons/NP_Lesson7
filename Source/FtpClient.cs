9c85beaef55d13f4dc141af57665eeb1eb9578c0
4214Lessons <112700431+4214Lessons@users.noreply.github.com>
2023-03-28 22:06:36 +0400

    finish

---
+++ Source/FtpClient.cs
@@ -0,0 +1,52 @@
+﻿#pragma warning disable
+
+
+using System.Net;
+
+
+class FtpClient
+{
+    private readonly string _server;
+    private readonly string _username;
+    private readonly string _password;
+
+    public FtpClient(string server, string username, string password)
+    {
+        _server = server;
+        _username = username;
+        _password = password;
+    }
+
+    public void UploadFile(string localPath, string remotePath)
+    {
+        using (var client = new WebClient())
+        {
+            client.Credentials = new NetworkCredential(_username, _password);
+            client.UploadFile($"ftp://{_server}/{remotePath}", "STOR", localPath);
+        }
+    }
+
+    public void DownloadFile(string remotePath, string localPath)
+    {
+        using (var client = new WebClient())
+        {
+            client.Credentials = new NetworkCredential(_username, _password);
+            client.DownloadFile($"ftp://{_server}/{remotePath}", localPath);
+        }
+    }
+
+    public string[] ListDirectory(string path = "/")
+    {
+        var request = WebRequest.Create($"ftp://{_server}/{path}") as FtpWebRequest;
+        request.Credentials = new NetworkCredential(_username, _password);
+        request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+        using (var response = request.GetResponse() as FtpWebResponse)
+        using (var stream = response.GetResponseStream())
+        using (var reader = new StreamReader(stream))
+        {
+            var listing = reader.ReadToEnd();
+            return listing.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("FTP");
+﻿#pragma warning disable
+
+using System.Net;
+
+
+// RequestFtpServer();
+// UploadFile();
+// DownloadFile();
+
+
+
+// var ftpClient = new FtpClient("localhost", string.Empty, string.Empty);
+// ftpClient.ListDirectory().ToList().ForEach(x => Console.WriteLine(x));
+
+
+
+void RequestFtpServer()
+{
+    var request = WebRequest.Create("ftp://localhost:21") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+    using var response = request.GetResponse() as FtpWebResponse;
+    using var stream = response.GetResponseStream();
+
+    using var reader = new StreamReader(stream);
+
+    var data = reader.ReadToEnd();
+    Console.WriteLine(data);
+}
+
+
+void UploadFile()
+{
+    var request = WebRequest.Create("ftp://localhost:21/uploadedFile.txt") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.UploadFile;
+
+    using var requestStream = request.GetRequestStream();
+
+    using var fs = new FileStream("destination.txt", FileMode.Open);
+    fs.CopyTo(requestStream);
+}
+
+
+void DownloadFile()
+{
+    var request = WebRequest.Create("ftp://localhost:21/source.txt") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+    using var response = request.GetResponse() as FtpWebResponse;
+    using var stream = response?.GetResponseStream();
+
+    using var fs = new FileStream("source.txt", FileMode.Create);
+    stream.CopyTo(fs);
+}
